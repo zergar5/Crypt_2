4232fc36ca01e6df5b3cfa5735221acf1aa16ac2
zergar5 <artem_kozhanov@mail.ru>
2022-10-05 22:37:51 +0700

    Доп

--- RSAEncoder.cs
+++ RSAEncoder.cs
@@ -5,7 +5,6 @@
         static public List<long> Encode(List<char> plainText, long n, long exp)
         {
             return plainText.Select(c => (long)Power.FastModPow(c, exp, n)).ToList();
-            //return (long)Power.FastModPow(plainText, exp, n);
         }
     }
 }
